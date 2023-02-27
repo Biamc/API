4b622c48f3fbae78cc56dd8a620d237aeb8841ec
Beatriz Machado <beatrizmcoutinho23@gmail.com>
2023-02-27 12:46:19 -0300

    construindo os params

--- src/serve.js
+++ src/serve.js
@@ -1,17 +1,25 @@
-// criar arquivo para importar o express. écomo se vc pegasse todas as funçoes do express e colocasse em uma constante
 
 const express = require('express')
 
-//criar outra constante para inicializar o express
 
 const app = express()
 
-//qual porta ele(express) ficara observando as requisiçoes
+app.get('/menssage/:id/:user', (request, response) => {
 
-// criar uma constante a qual vai poder atender as requisições(requests), e 
-//endereço de onde a API vai ficar observando as request e devolvendo as response
+  const { id, user } = request.params //desestruturação de request.params.user & request.params.id
+
+  response.send(`ID da menssagem ${id}. Parao usuário: ${user}`)
+})
+// '' -> coloca o endereço da rota
+//o expresse é quem ajuda a gerenciaras requesiçoes
+
+app.get('/users', (request, response) => {
+  const {page, limit} = request.query
+
+  response.send(`Página ${page}. Mostrar ${limit}`)
+})
+
 const PORT = 3333
 
 app.listen(PORT, () => console.log(`server is running on PORT ${PORT}`))
 
-//pode ser simplificado essa experiencia execuçaõ da aplicação
