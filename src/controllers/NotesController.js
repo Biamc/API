e469ceb40ed4a4f93d8208c04c2dc07a5e81c9e9
Beatriz Machado <beatrizmcoutinho23@gmail.com>
2023-03-10 17:55:21 -0300

    finalizando notes

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -1,9 +1,9 @@
 const knex = require('../database/knex')
 
 class NotesController{
-  async create(requeste, response) {
-    const {title, description, tags, links } = requeste.body
-    const { user_id } = requeste.params
+  async create(request, response) {
+    const {title, description, tags, links } = request.body
+    const { user_id } = request.params
 
     const note_id = await knex('notes').insert({
       title,
@@ -32,6 +32,77 @@ class NotesController{
 
     response.json()
 
+  }
+
+  async show(request, response){
+    const {id} = request.params
+
+    const note = await knex('notes').where({id}).first()
+    const tags = await knex('tags').where({note_id: id}).orderBy('name')
+    const links = await knex('links').where({note_id: id}).orderBy('created_at')
+
+    return response.json({
+      ...note,
+      tags,
+      links
+    })
+  }
+
+  async delete(request, response){
+    const {id} = request.params
+
+    await knex('notes'). where({id}).delete()
+
+    return response.json()
+  }
+
+  async index (request, response) {
+    const {title, user_id, tags} = request. query
+
+    let notes 
+
+    if (tags) {
+      const filterTags = tags.split(',').map(tag => tag.trim())
+
+      notes = await knex('tags')
+      .select([
+        'notes.id',
+        'notes.title',
+        'notes.user_id'
+      ])
+      .where('notes.user_id', user_id)
+      .whereLike('title', `%${title}%`)
+      .whereIn('name', filterTags)
+      .innerJoin('notes', 'notes.id', 'tags.note_id')
+      .orderBy('notes.title')
+
+        //spllit() converte o texto num array (',') utilizando como um delimitador a virgula
+        // map(tag => tag.trim() p filtrar apenas as tags
+        //whereIn() p fazer uma pesquisa baseado nas tags, passando o nome e o a palavra/tagque vc quer comparar
+
+        //select() dentro vc passa quais campos vc quer selecionar de ambas as taelas: nome-da-tabela.campo/notes.id
+
+    }else{
+     
+
+    notes = await knex('notes').where({user_id}).whereLike('title', `%${title}%`).orderBy('title')
+    // whereLike ajuda encontrar valores que contenham uma palavra por exemplo.o % serve para indicar que qualquer coisa que venha antesou depois sejalevado a mim
+    }
+
+    // p vincular as tags como user_id
+    const userTags = await knex('tags').where({user_id})//percorre as tags em que o user_id seja iguais/ que tenha o mesmo user_id
+    
+    const notesWithTags= notes.map(note => {
+      const noteTags = userTags.filter(tag => tag.note_id === note.id)
+     //percorre todas as notas e cria uma const noteTags a qual vai filtrar as tags que tenha o id da nota que seja vinculada a tag seja igual ao note.id
+    
+    return{
+      ...note,
+      tags: noteTags
+    }
+  })
+
+    return response.json(notesWithTags)//assim retorna as notas que possuem a mesma tag
   }
 }
 
--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -7,7 +7,11 @@ const notesRoutes = Router()
 
 const notesController = new NotesController()
 
+notesRoutes.get('/', notesController.index)
 notesRoutes.post('/:user_id', notesController.create)
+notesRoutes.get('/:id', notesController.show)
+
+notesRoutes.delete('/:id', notesController.delete)
 
 
 module.exports = notesRoutes //esta exportzando para quem utilizar este arquivo poder utilizar
